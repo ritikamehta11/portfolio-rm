9742108a60023fdb3d9180b9b7cdaac1a0aea6fb
Ritika Mehta <mehtaritika@gmail.com>
2025-12-24 09:53:19 -0500

    resolved error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.s vg'
 import './App.css'
 import { Hero } from './components/Hero/Hero'
  import { Skills } from './components/Skills/Skills'
